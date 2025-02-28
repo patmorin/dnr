ba16ac2cccc06920b2661a9a7563026311f3c931
Pat Morin <morin@scs.carleton.ca>
2025-02-28 05:31:50 -0500

    Finding the optimal choices of d and h

---
+++ optimize.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import numpy as np
+import math
+
+
+def bag_size1(t,d):
+    return (2*t-1)+d+1
+
+def bag_size2(t, d):
+    return 2*t*(2/3)**(d) + 5*d
+
+def check(t, d):
+    return 2*t*(2/3)**(d) + 4*d
+
+def optimal_t(d):
+    return (4*d) / (1-2*(2/3)**(d))
+
+
+# print("t(5) = {}".format(t(5)))
+
+# find miminum t such that 2*t*(2/3)**(d-1) + 4*d <= t
+a = [optimal_t(d) for d in range(20)]
+mn = -1
+for i in range(len(a)):
+  if a[i] > 0 and (mn == -1 or a[i] < a[mn]):
+      mn = i
+# print(a)
+print(mn, a[mn])
+
+topt = a[mn]
+print("Best (integer) solution: t = {}, h = {}, |B_x| = {}".format(topt, mn, check(topt, mn)))
+
+for d in range(mn+1):
+  print("Depth {}, |B_x| <= {}".format(d, min(bag_size1(topt,d), bag_size2(topt,d))))
+
+
+# dopt = np.argmin(a)
+#
+# dopt =
+#
+# print(a)
+# print(dopt+3, a[dopt])
+# topt = math.ceil(a[dopt])
+# print([min(tw1(topt,d), tw2(topt,d)) for d in range(dopt+4)])
