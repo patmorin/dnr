38b2493707bb72c9f7976c550b295c4339214213
Pat Morin <morin@scs.carleton.ca>
2025-03-18 09:46:57 -0400

    Pfft

--- optimize.py
+++ optimize.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import numpy as np
 import math
+from fractions import Fraction
 
-a = 2+1/6
+a = 2
 b = 3
 
 def bag_size1(t,d):
@@ -12,28 +13,27 @@ def bag_size2(t, d):
     return check(t,d) + d
 
 def check(t, d):
-    return 2*t*(2/3)**(d) + (b+1)*d
+    return 2*t*Fraction(2,3)**(d) + (b+1)*d
 
 def optimal_t(d):
-    return ((b+1)*d) / (1-a*(2/3)**(d))
+    return ((b+1)*d) / (1-a*Fraction(2,3)**(d))
 
 
 # print("t(5) = {}".format(t(5)))
 
 # find miminum t such that 2*t*(2/3)**(d-1) + 4*d <= t
 a = [optimal_t(d) for d in range(20)]
-mn = -1
+h = -1
 for i in range(len(a)):
-  if a[i] > 0 and (mn == -1 or a[i] < a[mn]):
-      mn = i
+  if a[i] > 0 and (h == -1 or a[i] < a[h]):
+      h = i
 # print(a)
-print(mn, a[mn])
+t = a[h]
+print("h = {}, t = {} ≈ {:.40f}".format(h, t, t))
 
-topt = a[mn]
-print("Best (integer) solution: t = {}, h = {}, |B_x| = {}".format(topt, mn, check(topt, mn)))
-
-for d in range(mn+1):
-  print("Depth {}, |B_x| <= {}".format(d, min(bag_size1(topt,d), bag_size2(topt,d))))
+for d in range(h+1):
+  bag_size = min(bag_size1(t,d), bag_size2(t,d))
+  print("Depth {}, |B_x| <= {} ≈ {:.40f}".format(d, bag_size, bag_size))
 
 
 # dopt = np.argmin(a)
