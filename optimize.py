600a9e1b120c1d5bf1fce53e06d6966d7ac860c5
Pat Morin <morin@scs.carleton.ca>
2025-03-20 09:00:18 -0400

    Pffft

--- optimize.py
+++ optimize.py
@@ -3,17 +3,17 @@ import numpy as np
 import math
 from fractions import Fraction
 
-a = 2
+a = 2 + Fraction(1,6)
 b = 3
 
 def bag_size1(t,d):
     return (2*t)+d+1
 
 def bag_size2(t, d):
-    return check(t,d) + d
+    return check(t,d) + 1
 
 def check(t, d):
-    return 2*t*Fraction(2,3)**(d) + (b+1)*d
+    return a*t*Fraction(2,3)**(d) + (b+1)*d
 
 def optimal_t(d):
     return ((b+1)*d) / (1-a*Fraction(2,3)**(d))
@@ -22,13 +22,13 @@ def optimal_t(d):
 # print("t(5) = {}".format(t(5)))
 
 # find miminum t such that 2*t*(2/3)**(d-1) + 4*d <= t
-a = [optimal_t(d) for d in range(20)]
+q = [optimal_t(d) for d in range(20)]
 h = -1
-for i in range(len(a)):
-  if a[i] > 0 and (h == -1 or a[i] < a[h]):
+for i in range(len(q)):
+  if q[i] > 0 and (h == -1 or q[i] < q[h]):
       h = i
 # print(a)
-t = a[h]
+t = q[h]
 print("h = {}, t = {} ≈ {:.40f}".format(h, t, t))
 
 for d in range(h+1):
